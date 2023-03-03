0dfb1ba4e7e68e648873eb36ec06e81a5ae75b07
ecinematic <ecinematic@yahoo.com>
2023-03-02 17:25:03 -0700

    pushing final work

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # ReadMeRainbow
+
+## Description
+Utilizing node to create a readme file
+
+## Installation
+
+node js
+
+## Usage
+
+To create a professional readme file
+
+## Credits
+Created by ecinematic 
+ecinematic@yahoo.com
+
+Additional credits: n/a
+
+## License
+
+undefined
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+n/a
+
+## Features
+
+n/a
+
+## How to Contribute
+
+for demo use only. no contributors
+
+## Tests
+
--- index.js
+++ index.js
@@ -1,13 +1,117 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { title } = require('process');
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const generateHTML = ({ title, description, installation, usage, credits, license, badges, features, contribute, name, email }) =>
+`# ${title}
 
-// TODO: Create a function to initialize app
-function init() {}
+## Description
+${description}
 
-// Function call to initialize app
-init();
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## Credits
+Created by ${name} 
+${email}
+
+Additional credits: ${credits}
+
+## License
+
+${license}
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+${badges}
+
+## Features
+
+${features}
+
+## How to Contribute
+
+${contribute}
+
+## Tests
+
+`;
+
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your GitHub user name?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project.',
+      },
+      {
+          type: 'input',
+          name: 'installation',
+          message: 'What command should be run to run the tests?',
+      },
+      {
+          type: 'input',
+          name: 'usage',
+          message: 'Explain the usage of this project.',
+      },
+      {
+          type: 'input',
+          name: 'credits',
+          message: 'Provide information on any outside credit.',
+      },
+      {
+        type: 'checkbox',
+        message: 'What license should your project have?',
+        name: 'stack',
+        choices: ['None', 'MIT', 'Apache 2.0', 'GNU v3', 'BSD 3-Clause', 'Mozilla Public License 2.0'],
+      },
+      {
+          type: 'input',
+          name: 'badges',
+          message: 'List any badges.',
+      },
+      {
+          type: 'input',
+          name: 'features',
+          message: 'List any features.',
+      },
+      {
+          type: 'input',
+          name: 'contribute',
+          message: 'How to contribute?',
+      },
+  ])
+  .then((answers) => {
+    const htmlPageContent = generateHTML(answers);
+
+    fs.writeFile('README.md', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created your README.md file!')
+    );
+  });
